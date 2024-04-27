6d2d4b4007d7e6d77d76b868356c213fec2a177a
Egor Kuziakov <egor.kuziakov@gmail.com>
2024-04-27 10:50:30 +0300

    Fix typing

--- app/presentation/fastapi/deps.py
+++ app/presentation/fastapi/deps.py
@@ -12,7 +12,7 @@ from app.infra.repositories import uow, Repository
 bearer_security = HTTPBearer(auto_error=False)
 
 
-def get_repository() -> typing.Generator[Repository]:
+def get_repository() -> typing.Generator[Repository, None, None]:
     with uow as repository:
         yield repository
 
